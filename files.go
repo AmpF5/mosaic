e32846d267980947a81132cb0be096ca6e5c8be6
AmpF5 <lolc44t@gmail.com>
2025-02-13 21:36:01 +0100

    Add saving file paths from dir

--- files.go
+++ files.go
@@ -1,27 +1,86 @@
 package main
 
 import (
-	"os/exec"
-	"runtime"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"slices"
+
+	"github.com/ncruces/zenity"
 )
 
-func OpenFileExplorer() {
-	var cmd *exec.Cmd
+var mimeTypes = []string{"jpg", "png", "jpeg", "heic"}
 
-	switch runtime.GOOS {
-	case "windows":
-		cmd = exec.Command("explorer")
-	case "darwin":
-		cmd = exec.Command("open", ".")
-	case "linux":
-		cmd = exec.Command("xdg-open", ".")
-	default:
-		return
+type file struct {
+	filePath string
+	mimeType string
+	size     int
+}
+
+func createFile(filePath, mimeType string, size int) *file {
+	f := file{
+		filePath: filePath,
+		mimeType: mimeType,
+		size:     size,
 	}
 
-	// Run the command in the background
-	err := cmd.Start()
+	return &f
+}
+
+func (f file) String() string {
+	return fmt.Sprintf("{ file_path: %s, mime_type: .%s, size: %d }", f.filePath, f.mimeType, f.size)
+}
+
+func readDir(path string) {
+	files, err := os.ReadDir(path)
+
 	if err != nil {
-		println("Error:", err.Error())
+		log.Fatal(err)
 	}
+
+	images := []file{}
+
+	for _, file := range files {
+		fileInfo, err := file.Info()
+		if err != nil {
+			return
+		}
+
+		if fileInfo.IsDir() {
+			continue
+		}
+
+		mimeType, founded := getFileMimetype(fileInfo.Name(), mimeTypes)
+
+		if !founded {
+			continue
+		}
+
+		image := createFile(fileInfo.Name(), mimeType, int(fileInfo.Size()))
+		images = append(images, *image)
+	}
+
+	for _, image := range images {
+		fmt.Println(image)
+	}
+}
+
+func getFileMimetype(fileName string, mimeTypes []string) (string, bool) {
+	r, _ := regexp.Compile("([a-z]+)$")
+
+	mimeType := r.FindString(fileName)
+
+	return mimeType, slices.Contains(mimeTypes, mimeType)
+}
+
+func OpenFileExplorer() {
+	dir, err := zenity.SelectFile(zenity.Directory())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	readDir(dir)
+
+	fmt.Println("Selected directory:", dir)
 }
